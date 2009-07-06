5a8e2f726f8dc2606e11668a09be163bc8b4b0e6
Charles Cazabon <charlesc-getmail@pyropus.ca>
2009-07-06 14:26:05 -0400

    v4.10.0
    - add "message_log_verbose" option, defaulting to false.  If true, the
      message_log file (and syslog, if used) will contain a little more information
      than the default of just messages retrieved.  Thanks: Matthias Andree,
      Gregory Morozov.

--- getmail-4.10.0/getmailcore/__init__.py
+++ getmail-4.10.0/getmailcore/__init__.py
@@ -6,7 +6,7 @@ simple and multidrop POP3 mailboxes, multidrop SDPS mailboxes, simple and
 multidrop IMAP mailboxes.  Also supports POP3- and IMAP-over-SSL, message
 filtering, and other features.
 
-getmail is Copyright (C) 1998-2007 Charles Cazabon.  All rights reserved.
+getmail is Copyright (C) 1998-2009 Charles Cazabon.  All rights reserved.
 Distributed under the terms of the GNU General Public License version 2 (only).
 You should have received a copy of the license in the file COPYING.
 '''
@@ -16,7 +16,7 @@ if sys.hexversion < 0x2030300:
     raise ImportError('getmail version 4 requires Python version 2.3.3'
                       ' or later')
 
-__version__ = '4.9.2'
+__version__ = '4.10.0'
 
 __all__ = [
     'baseclasses',
