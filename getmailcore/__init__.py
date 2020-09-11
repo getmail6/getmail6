19f6b8500b7c5d4ca6c02a36c9113928e202dba1
Roland Puntaier <roland.puntaier@gmail.com>
2020-09-11 17:47:57 +0200

    fix #37: update copyright date and notices

--- getmailcore/__init__.py
+++ getmailcore/__init__.py
@@ -5,14 +5,14 @@ simple and multidrop POP3 mailboxes, multidrop SDPS mailboxes, simple and
 multidrop IMAP mailboxes.  Also supports POP3- and IMAP-over-SSL, message
 filtering, and other features.
 
-getmail is Copyright (C) 1998-2019 Charles Cazabon.  All rights reserved.
+getmail is Copyright (C) 1998-2020 Charles Cazabon and others. All rights reserved.
 Distributed under the terms of the GNU General Public License version 2 (only).
 You should have received a copy of the license in the file COPYING.
 '''
 
 import sys
 
-__version__ = '6.7'
+__version__ = '6.8'
 
 __py_required__ = '2.7.18'
 __py_required_hex__ = 0x20712f0
