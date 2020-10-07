281423b98a94f12c21c801e6276f2eab6c51d2ff
Roland Puntaier <roland.puntaier@gmail.com>
2020-10-07 12:00:27 +0200

    blurp back to two, as log object not one, either

--- getmailcore/__init__.py
+++ getmailcore/__init__.py
@@ -17,11 +17,6 @@ __license__ = 'GNU GPL version 2'
 __py_required__ = '2.7.18'
 __py_required_hex__ = 0x20712f0
 
-def blurb():
-    log.info('getmail version %s\n' % __version__)
-    log.info('Copyright (C) 1998-2020 Charles Cazabon and others. '
-             'Licensed under %s.'%__license__)
-
 if sys.hexversion < __py_required_hex__:
     raise ImportError('getmail version %s requires Python version %s '
                       'or later'%(__version__,__py_required__))
