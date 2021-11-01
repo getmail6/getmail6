525e8d4824978c6f6384643f1f73108748a43f53
Roland Puntaier <roland.puntaier@gmail.com>
2021-11-01 08:33:57 +0100

    reinstantiate copyright notice in __init__.py mistakenly dropped (first moved to blurb() then blurb() moved to different file)

--- getmailcore/__init__.py
+++ getmailcore/__init__.py
@@ -8,6 +8,10 @@ getmail is a reliable, modular, extensible mail retriever with support for
 simple and multidrop POP3 mailboxes, multidrop SDPS mailboxes, simple and
 multidrop IMAP mailboxes.  Also supports POP3- and IMAP-over-SSL, message
 filtering, and other features.
+
+getmail is Copyright (C) 1998-2019 Charles Cazabon.  All rights reserved.
+Distributed under the terms of the GNU General Public License version 2 (only).
+You should have received a copy of the license in the file COPYING.
 '''
 
 import sys
